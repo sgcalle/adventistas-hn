1c09813d44aef0169a13b7cda627034026a7cf6a
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-03-06 10:44:12 +0100

    Invoice updated

--- honduras_invoices/__manifest__.py
+++ honduras_invoices/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "adventistas_reports",
+    'name': "Honduras Invoices",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """ Allow you make honduras invoices """,
 
     'description': """
-        Long description of module's purpose
+        Honduras Invoices and Reports, this add min and max invoice number,
+        issue limit date.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "EduwebGroup",
+    'website': "http://www.eduwebgroup.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Accounting',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
