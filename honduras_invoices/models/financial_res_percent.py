63c4727ce3390de34c788785f12d1477a94076c8
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-03-27 14:24:04 +0100

    Fixed bug and added followers automatically

--- honduras_invoices/models/financial_res_percent.py
+++ honduras_invoices/models/financial_res_percent.py
@@ -7,7 +7,6 @@ class FinacialResponsabilityPercent(models.Model):
     _description = "Realted model to finance responsabilty"
 
     partner_id = fields.Many2one("res.partner", string="Customer", domain=[("is_family", "=", False)])
-    # partner_family_ids = fields.Many2many(related="partner_id.family_ids")
 
     family_id = fields.Many2one("res.partner", required=True, string="Family", domain=[("is_family", "=", True), ('is_company', '=', True)])
     category_id = fields.Many2one("product.category", required=True, string="Category", domain=[("parent_id", "=", False)])
@@ -16,9 +15,5 @@ class FinacialResponsabilityPercent(models.Model):
     @api.onchange('family_id')
     def _get_family_domain(self):
         self.ensure_one()
-        self.family_id = False
         family_ids = self.partner_id.family_ids.ids
-        if family_ids:
-            return  {'domain':{'family_id':[('id', 'in', family_ids)]}}
-        else:
-            return  {'domain':{'product_id':[('id', 'in', [])]}}
+        return  {'domain':{'family_id':[('id', 'in', family_ids)]}}
