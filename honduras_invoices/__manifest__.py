9884189179348b4517788680e063c859f83ff642
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-04-08 15:13:18 +0200

    Payment Recipment improved

--- honduras_invoices/__manifest__.py
+++ honduras_invoices/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting',
-    'version': '0.3-beta',
+    'version': '0.5',
 
     # any module necessary for this one to work correctly
     'depends': [
