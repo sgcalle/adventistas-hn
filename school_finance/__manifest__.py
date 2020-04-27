108e0f80c893937d3d66d6ff750d27337e9c573a
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-04-27 14:52:05 +0200

    Updated

--- school_finance/__manifest__.py
+++ school_finance/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Invoicing',
-    'version': '0.3.1',
+    'version': '0.4',
 
     # any module necessary for this one to work correctly
     'depends': [
@@ -32,9 +32,5 @@
         'views/views.xml',
         'views/templates.xml',
         'wizard/res_partner_make_sale.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
