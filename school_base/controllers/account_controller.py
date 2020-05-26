9ac750df8f936584bf9f01f66a7f3619fdefcb58
David Conde <dconde@eduwebgroup.com>
2020-05-26 09:46:55 +0000

    Adv datos fact 04

--- school_base/controllers/account_controller.py
+++ school_base/controllers/account_controller.py
@@ -17,10 +17,10 @@ class StudentController(http.Controller):
         #crea una variable con el modelo desde donde se va a tomar la información 
         # adm_uni.application
         # status_type
-        students = http.request.env['adm_uni.application']        
+        students = http.request.env['res.partner']#adm_uni.application']        
         
         #filtro del modelo basados en parametros de la url 
-        search_domain = [("status_type","=","stage")]
+        search_domain = []#("status_type","=","stage")]
         #search_domain = [("status_type","=","fact_integration")] #,("country_id", "=", int(params['country_id']))] if "country_id" in params else []
         #search_domain = [("status_type","=","fact_integration"),("country_id", "=", int(params['country_id']))] 
         
@@ -28,46 +28,9 @@ class StudentController(http.Controller):
         students_record = students.search(search_domain)      
         
         #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
-        students_values = students_record.read(["id","city","country_id","state_id", "street_address","zip","first_name","middle_name","last_name","name","email", "birthdate","gender","phone", "status_id","status_type","current_school","current_school_address","create_date","create_uid","write_date","write_uid"])
+        students_values = students_record.read(["id"]#,"city","country_id","state_id", "street_address","zip","first_name","middle_name","last_name","name","email", "birthdate","gender","phone", "status_id","status_type","current_school","current_school_address","create_date","create_uid","write_date","write_uid"])
 
-        # Se recorre por cada estudiante
-        for record in students_values: 
-            # Convertir fechas a string
-            if record["birthdate"]:
-                record["birthdate"] = record["birthdate"].strftime('%m/%d/%Y')
-            else:
-                record["birthdate"] = '' 
-            # Es lo mismo que:
-            # date_of_birth = record["birthdate"]
-            # date_of_birth = date_of_birth.strftime('%m/%d/%Y')
-            # record["birthdate"] = date_of_birth 
-            
-            if record["create_date"]:
-                record["create_date"] = record["create_date"].strftime('%m/%d/%Y')
-            else:
-                record["create_date"] = ''                
-            
-            if record["write_date"]:
-                record["write_date"] = record["write_date"].strftime('%m/%d/%Y')
-            else:
-                record["write_date"] = ''
-                
-            # record["__last_update"] = record["__last_update"].strftime('%m/%d/%Y')
-            # record["create_date"] = record["create_date"].strftime('%m/%d/%Y')
-            # record["write_date"] = record["write_date"].strftime('%m/%d/%Y')   
-            
-            #crea una variable con el modelo desde donde se va a tomar la información
-            attachments = http.request.env['ir.attachment']        
-        
-            #filtro del modelo basados en parametros de la url
-            search_domain_attach = [("res_model", "=", "adm_uni.application"),("res_id","=",record["id"])]
-        
-            #Tomar informacion basado en el modelo y en el domain IDS
-            attachments_record = attachments.search(search_domain_attach)      
-        
-            #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
-            attachments_values = attachments_record.read(["id","name"])    
-            record["attachIds"] = json.dumps(attachments_values)
+
             
         #students_values.append("test")
         #pintar la información obtenida, esto lo utilizamos para parsearlo en el ajax.         
