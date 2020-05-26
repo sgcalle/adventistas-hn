2491311fb0a36b3777106be423ac77f77bc02a07
David Conde <dconde@eduwebgroup.com>
2020-05-26 08:49:36 +0000

    Prueba datos fact 01

--- school_base/controllers/account_controller.py
+++ school_base/controllers/account_controller.py
@@ -104,23 +104,73 @@ class StudentController(http.Controller):
     
     
     #definiendo la url desde donde va ser posible acceder, tipo de metodo, cors para habiltiar accesos a ip externas.
-    @http.route("/account/getDataOdooFromFamilyID", auth="public", methods=["POST"], cors='*', csrf=False)
-    # define una funcion principal 
-    def insertId(self, **kw):  
-        data = json.loads(kw["data"])
+    @http.route("/account/getDataOdooFromFamilyID", auth="public", methods=["GET"], cors='*', csrf=False)
+    # define una funcion principal
+    def datosFact(self, **kw):                         
+        #{"id": 17, "first_name": "Luis"}
+        #data = '[{"id": 16}]'
+                
+        #data = json.loads(data)
+        #data = json.loads(kw["data"])        
+       
         
-        students = http.request.env['account.move']        
+
         
-        #filtro del modelo basados en parametros de la url 
-        search_domain = [("partner_id","=",data["id"])]
+       
+        students = http.request.env['account.invoice']        
+        #students = http.request.env['account.invoice']
+
+        
+        #filtro del modelo basados en parametros de la url
+#        search_domain = [("partner_id","=",int(kw['id']))] #if "id" in kw else []
+        
+        id = kw["fact_id"] if "fact_id" in kw else kw["id"]
+        search_domain = [("facts_id","=",int(id))]         
+        
         #search_domain = [("status_type","=","fact_integration")] #,("country_id", "=", int(params['country_id']))] if "country_id" in params else []
-        #search_domain = [("status_type","=","fact_integration"),("country_id", "=", int(params['country_id']))] 
+        #search_domain = [("status_type","=","fact_integration"),("country_id", "=", int(params['country_id']))]        
+        
+        #if kw['id'] != None: 
         
         #Tomar informacion basado en el modelo y en el domain IDS
         students_record = students.search(search_domain)      
+
+        #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior        
+        students_values = students_record.read(["access_token","amount_total","date_invoice","date_due","payment_term_id","user_id","invoice_line_ids"])
         
-        #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
-        students_values = students_record.read(["access_token","amount_total","invoice_date"])
-        
-        
+        for record in students_values: 
+            if record["date_invoice"]:
+                record["date_invoice"] = record["date_invoice"].strftime('%m/%d/%Y')
+            else:
+                record["date_invoice"] = ''                
+
+            if record["date_due"]:
+                record["date_due"] = record["date_due"].strftime('%m/%d/%Y')
+            else:
+                record["date_due"] = ''                
+                
+            record["datosLinea"] = []
+            
+#            for lineas in invoice_line_id:           
+            
+                #crea una variable con el modelo desde donde se va a tomar la información
+            datosLinea = http.request.env['account.invoice.line']        
+
+                #filtro del modelo basados en parametros de la url 
+            search_domain_attach = [("invoice_id","=",record["id"])]
+
+                #Tomar informacion basado en el modelo y en el domain IDS
+            datosLinea_record = datosLinea.search(search_domain_attach)      
+
+                #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
+            datosLinea_values = datosLinea_record.read(["product_id","quantity"]) 
+                
+ #           record["datosLinea"] = json.dumps(datosLinea_values)         
+
+            record["datosLinea"] = datosLinea_values
+                
+
         return json.dumps(students_values)
+
+
+#if(row[1] != None and row[2] != None):
