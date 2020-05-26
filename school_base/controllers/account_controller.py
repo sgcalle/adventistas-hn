6824248c54a7b783768adf0a3f9ce04e3be2b4e4
David Conde <dconde@eduwebgroup.com>
2020-05-26 09:17:53 +0000

    Adv datos fact 01

--- school_base/controllers/account_controller.py
+++ school_base/controllers/account_controller.py
@@ -155,7 +155,7 @@ class StudentController(http.Controller):
             datosLinea = http.request.env['account.invoice.line']        
 
                 #filtro del modelo basados en parametros de la url 
-            search_domain_attach = [("invoice_id","=",record["id"])]
+            search_domain_attach = [("invoice_id","=",record["id"])] 
 
                 #Tomar informacion basado en el modelo y en el domain IDS
             datosLinea_record = datosLinea.search(search_domain_attach)      
