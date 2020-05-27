5a1834cb9e4dd7f0b08be115cef317d48e3ed44c
rainierking <rainier.king@odevsolutions.com>
2020-05-27 15:12:35 +0000

    Initial commit of l10n_hn_check_printing module

---
+++ l10n_hn_check_printing/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Honduras Checks Layout',
+
+    'summary': """ Print HN Checks """,
+
+    'description': """
+        This module allows to print your payments on pre-printed checks.
+    """,
+
+    'author': 'Eduwebgroup',
+    'website': 'http://www.eduwebgroup.com',
+
+    'category': 'Invoicing',
+    'version': '0.1',
+
+    'depends': [
+        'account_check_printing',
+    ],
+
+    'data': [
+        'data/hn_check_printing.xml',
+        'report/print_check.xml',
+        'report/print_check_top.xml',
+        'report/print_check_middle.xml',
+        'report/print_check_bottom.xml',
+    ],
+}
