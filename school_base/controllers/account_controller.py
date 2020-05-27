9b0b9f3ddbbe49fddeb7990178859df2b2e76ce9
David Conde <dconde@eduwebgroup.com>
2020-05-27 05:24:55 +0000

    Fact 13 datos fact 03

--- school_base/controllers/account_controller.py
+++ school_base/controllers/account_controller.py
@@ -142,27 +142,7 @@ class StudentController(http.Controller):
             else:
                 record["date_invoice"] = ''               
 
-               
-                
-            record["datosLinea"] = []
-            
-#            for lineas in invoice_line_id:           
-             
-                #crea una variable con el modelo desde donde se va a tomar la información
-            datosLinea = http.request.env['account.move.line']        
-
-                #filtro del modelo basados en parametros de la url 
-            search_domain_attach = [("invoice_id","=",record["id"])]
-
-                #Tomar informacion basado en el modelo y en el domain IDS
-            datosLinea_record = datosLinea.search(search_domain_attach)      
-
-                #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
-            datosLinea_values = datosLinea_record.read(["product_id","quantity"]) 
-                
- #           record["datosLinea"] = json.dumps(datosLinea_values)         
-
-            record["datosLinea"] = datosLinea_values
+          
                 
 
         return json.dumps(students_values)
