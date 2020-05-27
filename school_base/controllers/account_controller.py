9d671bb8291af4ca81f3223182dc7445ab5393b7
David Conde <dconde@eduwebgroup.com>
2020-05-27 07:41:53 +0000

    Fact 13 datos fact 17

--- school_base/controllers/account_controller.py
+++ school_base/controllers/account_controller.py
@@ -134,7 +134,7 @@ class StudentController(http.Controller):
         students_record = students.search(search_domain)       
 
         #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior       "invoice_payment_term_id", 
-        students_values = students_record.read(["partner_id","access_token","amount_total","invoice_date","invoice_line_ids"])
+        students_values = students_record.read(["partner_id","access_token","amount_total","invoice_date","journal_id","company_id","invoice_line_ids"])
         
         for record in students_values: 
             if record["invoice_date"]:
@@ -145,18 +145,16 @@ class StudentController(http.Controller):
                 
             record["datos"] = []
             
-            for lineas in invoice_line_id: 
-            
-                #crea una variable con el modelo desde donde se va a tomar la información
-                datosLinea = http.request.env['account.move.line']        
-                #filtro del modelo basados en parametros de la url 
-                search_domain_attach = [("move_id","=",lineas["invoice_line_id"])]
-                #Tomar informacion basado en el modelo y en el domain IDS
-                datosLinea_record = datosLinea.search(search_domain_attach)      
-                #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
-                datosLinea_values = datosLinea_record.read(["product_id","quantity"]) 
-
-                record["datos"] = datosLinea_values
+            #crea una variable con el modelo desde donde se va a tomar la información
+            datosLinea = http.request.env['account.move.line']        
+            #filtro del modelo basados en parametros de la url 
+            search_domain_linea = [("move_id","=",record["id"])]
+            #Tomar informacion basado en el modelo y en el domain IDS
+            datosLinea_record = datosLinea.search(search_domain_linea)      
+            #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
+            datosLinea_values = datosLinea_record.read(["product_id","quantity"]) 
+ 
+            record["datos"] = datosLinea_values
                 
 
         return json.dumps(students_values)
