4f0ab89657efb95e7f8bf2b20c8e3fb6a80f187f
David Conde <dconde@eduwebgroup.com>
2020-05-28 06:39:19 +0000

    Datos factura 07

--- school_base/controllers/account_controller.py
+++ school_base/controllers/account_controller.py
@@ -157,7 +157,8 @@ class StudentController(http.Controller):
             #Tomar informacion basado en el modelo y en el domain IDS
             datosLinea_record = datosLinea.search(search_domain_linea)      
             #Obtienes la información basada en los ids anteriores y tomando en cuenta los campos definifos en la funcion posterior
-            datosLinea_values = datosLinea_record.read(["product_id","quantity","price_unit","discount","analytic_tag_ids","subscription_id","account_id","tax_ids"]) 
+            datosLinea_values = datosLinea_record.read(["product_id","quantity","price_unit","discount","analytic_tag_ids","subscription_id","account_id","tax_ids",
+                                                       "analytic_account_id","name"]) 
  
             record["datos"] = datosLinea_values
                 
