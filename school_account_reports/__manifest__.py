26019ed2e7366214cb0cce8cf0c8805053ee81af
rainierking <rainier.king@odevsolutions.com>
2020-05-29 06:23:49 +0000

    Initial commit of school_account_reports module

---
+++ school_account_reports/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'School Accounting Reports',
+
+    'summary': """ Accounting reports for schools """,
+
+    'description': """ Accounting reports for schools """,
+
+    'author': 'Eduwebgroup',
+    'website': 'http://www.Eduwebgroup.com',
+
+    'category': 'Accounting',
+    'version': '0.1',
+
+    'depends': [
+        'account_reports',
+        'school_finance',
+    ],
+
+    'data': [
+        'views/account_student_ledger_templates.xml',
+        'views/account_student_ledger_views.xml',
+        'views/res_partner_views.xml',
+    ],
+}
