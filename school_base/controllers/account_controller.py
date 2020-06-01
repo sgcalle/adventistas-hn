37f3e94b20c8a31b5633077c751938ffc1400e4d
DavidCondeCubas <dconde@eduwebgroup.com>
2020-06-01 06:18:03 +0000

    change type to admin

--- school_base/controllers/account_controller.py
+++ school_base/controllers/account_controller.py
@@ -106,7 +106,7 @@ class StudentController(http.Controller):
     
     #metodo encargado de recuperar datos de una factura y enviarla a FACTS
     #definiendo la url desde donde va ser posible acceder, tipo de metodo, cors para habiltiar accesos a ip externas.
-    @http.route("/account/getDataOdooFromFamilyID", auth="public", methods=["GET"])#, cors='*', csrf=False)
+    @http.route("/account/getDataOdooFromFamilyID", auth="admin", methods=["GET"])#, cors='*', csrf=False)
     # define una funcion principal
     def datosFact(self, **kw):         
         
