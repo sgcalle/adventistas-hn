ee0082939cdcb363244fab4a8b07555446c48471
DavidCondeCubas <dconde@eduwebgroup.com>
2020-06-01 06:10:57 +0000

    change type to public

--- school_base/controllers/states_controller.py
+++ school_base/controllers/states_controller.py
@@ -6,10 +6,11 @@ import json
 
 
 class StateController(http.Controller):
-    @http.route("/admission/states", auth="admin", methods=["GET"])
+    @http.route("/admission/states", auth="public", methods=["GET"])
     def get_states(self, **params):
         #Codigo para filtrar por el districtCode que llega en la URL. Solo queremos las facturas de ese districtCode
-        #crea una variable con el modelo desde donde se va a tomar la información:'res.company'          
+        #crea una variable con el modelo desde donde se va a tomar la información:'res.company'   
+        #
         compania = http.request.env['res.company']
         #filtro del modelo basados en parametros de la url.
         search_compania = [("x_district_code","=",(kw['dist']))]        
