a001c7a4834c04d4d1053bde066f73084ed61ce2
rainierking <rainierking@gitlab.com>
2020-06-09 10:18:16 +0800

    Add layout for not preprinted; Allow choosing check template in journal

--- l10n_hn_check_printing/models/account_payment.py
+++ l10n_hn_check_printing/models/account_payment.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api
+from odoo import models, api, _
+from odoo.exceptions import UserError
 
 class account_payment(models.Model):
     _inherit = "account.payment"
@@ -9,6 +10,9 @@ class account_payment(models.Model):
         if self:
             check_layout = self[0].company_id.account_check_printing_layout
             if check_layout != 'disabled' and self[0].journal_id.company_id.country_id.code == 'HN':
-                self.write({'state': 'sent'})
-                return self.env.ref('l10n_hn_check_printing.%s' % check_layout).report_action(self)
+                if self[0].journal_id.check_template_id:
+                    self.write({'state': 'sent'})
+                    return self.env.ref('l10n_hn_check_printing.action_print_check').report_action(self)
+                else:
+                    raise UserError(_("You have to choose a check layout. For this, go to the journal settings and set one."))
         return super(account_payment, self).do_print_checks()
