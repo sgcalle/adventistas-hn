a7aed2e88e944f6c069aeb27e5a3d507bebff37b
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-06-12 08:05:15 +0200

    Update discounts and finance

--- multiple_discounts/models/sale_order.py
+++ multiple_discounts/models/sale_order.py
@@ -1,72 +1,62 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, _
+from odoo import models, fields, _, api
 
 
 def get_parent_category(category_id):
     single_list = [category_id]
+
     if category_id.parent_id:
         single_list.extend(get_parent_category(category_id.parent_id))
         return single_list
     else:
         return single_list
 
+class SaleOrder(models.Model):
 
-def apply_discount(move_id, discount_ids):
-    write_lines = []
-    for invoice_line in move_id.invoice_line_ids:
-        invoice_line_categories = get_parent_category(
-            invoice_line.product_id.categ_id)
-        discount_applicable = discount_ids.filtered(
-            lambda discount: discount.category_id in invoice_line_categories)
+    _inherit = "sale.order"
 
-        for discount in discount_applicable:
-            percent = discount.percent
-            discount_count = -invoice_line.price_total * (percent/100)
+    def apply_discount(self):
 
-            write_lines.append(
-                (0, 0, {
-                    "product_id": discount.product_id.get_single_product_variant().get("product_id", False),
-                    "price_unit": discount_count,
-                    "analytic_account_id": discount.analytic_account_id.id,
+        for order_id in self:
+            write_lines = []
+            discount_ids = order_id.partner_id.discount_ids
+
+            for order_line_id in order_id.order_line:
+                invoice_line_categories = get_parent_category(order_line_id.product_id.categ_id)
+                discount_applicable = discount_ids.filtered(
+                    lambda discount: discount.category_id in invoice_line_categories)
+
+                for discount in discount_applicable:
+                    percent = discount.percent
+                    discount_count = -order_line_id.price_subtotal * (percent/100)
+
+                    order_line_create = {
+                        "product_id": discount.product_id.get_single_product_variant().get("product_id", False),
+                        "price_unit": discount_count,
+                        # "analytic_account_id": discount.analytic_account_id.id,
                     }
-                )
-            )
 
-    move_id.write({
-        "invoice_line_ids": write_lines
-    })
+                    if order_line_id.tax_id:
+                        order_line_create.update({"tax_id": [(6, 0, order_line_id.tax_id.ids)]})
 
 
+                    write_lines.append((0, 0, order_line_create))
 
-class SaleOrder(models.TransientModel):
-    _inherit = "sale.advance.payment.inv"
+            order_id.write({
+                "order_line": write_lines
+            })
 
-    def create_invoices(self):
-        moves = super(SaleOrder, self).create_invoices()
-        if moves.get("name", "") == "Invoices":
-            res_ids = []
 
-            # If res is 0 there are more than one invoice, so
-            # we have to find the ids in the domain
-            if moves["res_id"] != 0:
-                res_ids.append(moves["res_id"])
-            else:
-                id_domain = list(
-                    filter(lambda condition: condition[0] == "id", moves.get("domain", [])))[0]
-                if id_domain:
-                    ids = id_domain[2]
-                    res_ids = ids
+    @api.model
+    def create(self, vals):
+        order_ids = super().create(vals)
 
-            move_ids = self.env["account.move"].browse(res_ids)
-            sale_ids = self.env["sale.order"].browse(
-                self._context.get("active_ids", []))
+        # We add the discounts here if context's apply_discounts variable is set True
+        for order_id in order_ids:
 
-            # Apply discount to every move
-            for move in move_ids:
-                sale_order = sale_ids.filtered(
-                    lambda order: move in order.invoice_ids)
-                discount_ids = sale_order.partner_id.discount_ids
+            # 2020/06/12: We don't do it due we want to be applied every time
+            #  if self._context.get("apply_discounts", False):
+            order_id.apply_discount()
 
-                move.mapped(lambda move: apply_discount(move, discount_ids))
-        return moves
+        return order_ids
