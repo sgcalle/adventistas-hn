601fd443ba51ab1ce9cbfd93ec571cc9e21d6c0b
rainierking <rainierking@gitlab.com>
2020-07-10 15:23:28 +0800

    Add groups for non-public fields in hr.employee

--- hr_payroll_account_extends/models/hr_employee.py
+++ hr_payroll_account_extends/models/hr_employee.py
@@ -6,8 +6,11 @@ class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
     payroll_bill_product_id = fields.Many2one(string="Payroll Bill Product",
-        comodel_name="product.product")
+        comodel_name="product.product",
+        groups="hr.group_hr_user")
     payroll_invoice_product_id = fields.Many2one(string="Payroll Invoice Product",
-        comodel_name="product.product")
+        comodel_name="product.product",
+        groups="hr.group_hr_user")
     payroll_journal_id = fields.Many2one(string="Payroll Journal",
-        comodel_name="account.journal")
+        comodel_name="account.journal",
+        groups="hr.group_hr_user")
