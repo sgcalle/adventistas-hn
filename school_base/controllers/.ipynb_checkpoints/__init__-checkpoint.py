4049d0efea12293110aa3b8b142f4478db409851
LuisAngelMalaveMora@gmail.com <Zelda220402>
2020-07-24 12:15:01 +0200

    Fix school base

--- school_base/controllers/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import portal_controller
-from . import account_controller
--- school_base/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import portal_controller
-from . import account_controller
-from . import states_controller
