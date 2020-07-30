5be1263a7e04762421ab3f2cb5fab7aa148e11b4
LuisAngelMalaveMora@gmail.com <Zelda220402>
2020-07-30 15:17:30 +0200

    feat school_finance: added student grade level and homeroom in moves

--- school_finance/models/account_move.py
+++ school_finance/models/account_move.py
@@ -2,7 +2,9 @@
 
 from odoo import fields, models
 
+
 class AccountMove(models.Model):
+    """ Added school finance functionalities """
     _inherit = "account.move"
 
     student_id = fields.Many2one("res.partner", string="Student", domain=[('person_type', '=', 'student')])
@@ -12,10 +14,26 @@ class AccountMove(models.Model):
 
     receivable_account_id = fields.Many2one("account.account", string="Receivable account", domain=[("user_type_id.type", "=", "receivable")])
 
+    def _compute_grade_level(self):
+        for move_id in self:
+            move_id.student_grade_level = move_id.student_id.grade_level_id
+
+    student_grade_level = fields.Many2one("school_base.grade_level",
+                                          compute="_compute_grade_level",
+                                          string="Grade level", store=True)
+
+    def _compute_student_homeroom(self):
+        for move_id in self:
+            move_id.student_homeroom = move_id.student_id.homeroom
+
+    student_homeroom = fields.Char(compute="_compute_student_homeroom", string="Student homeroom", store=True)
+
     def set_receivable_account(self):
+        """ It uses receivable_account_id field to set autoamtically the receivable account """
         for record in self:
-            receivable_line_id = record.line_ids.filtered(lambda record: record.account_id.user_type_id.type == 'receivable')
+            receivable_line_id = record.line_ids.filtered(
+                lambda line: line.account_id.user_type_id.type == 'receivable')
             receivable_line_id.ensure_one()
-            if (receivable_line_id and record.receivable_account_id):
+            if receivable_line_id and record.receivable_account_id:
                 receivable_line_id.account_id = record.receivable_account_id.id
 
