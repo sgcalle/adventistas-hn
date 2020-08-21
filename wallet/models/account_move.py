52ac4bfe3d4893e3df7a6019ca547f0c7d17228d
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-08-21 18:06:22 +0200

    Trying to fix wallet credit limit check #3

--- wallet/models/account_move.py
+++ wallet/models/account_move.py
@@ -163,7 +163,7 @@ class AccountMove(models.Model):
                                           credit_note_id.id, credit_note_id.name, credit_note_id.amount_total))
                             move_id.js_assign_outstanding_line(receivable_line_id.id)
 
-            wallet_ids = wallet_payment_dict.keys()
+            wallet_ids = self.env["wallet.category"].browse(set(map(lambda wallet_id: wallet_id.id, wallet_payment_dict.keys())))
             wallet_ids.sorted("parent_count", reverse=True)
             for wallet_id in wallet_ids:
                 wallet_amount = wallet_id.get_wallet_amount(partner_id)
