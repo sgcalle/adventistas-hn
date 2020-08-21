47a424e0ecb69499adb319a808fc9410ee6d54a8
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-08-21 18:18:15 +0200

    fix (wallet): credit limit now is used in pay with wallet

--- wallet/__manifest__.py
+++ wallet/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Wallet',
-    'version': '1.0-beta2',
+    'version': '1.0-beta3',
 
     # any module necessary for this one to work correctly
     'depends': [
