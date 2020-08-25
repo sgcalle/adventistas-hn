d48b2e55cf824894a26a2cc29c0c3e9d52b65a65
rainierking <rainierking@gitlab.com>
2020-08-25 15:53:05 +0800

    [IMP] canteen: add field for canteen order date; add ui and pdf reports

--- canteen/models/sale_order.py
+++ canteen/models/sale_order.py
@@ -6,3 +6,4 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     is_canteen_order = fields.Boolean(string="Is Canteen Order")
+    canteen_order_date = fields.Date(string="Canteen Order Date")
