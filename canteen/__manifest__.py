cd6f7ecbd21a6092647f3faf4d85bebb824f2372
rainierking <rainierking@gitlab.com>
2020-08-25 15:53:05 +0800

    [IMP] canteen: add field for canteen order date; add ui and pdf reports

--- canteen/__manifest__.py
+++ canteen/__manifest__.py
@@ -23,9 +23,14 @@
 
     'data': [
         'security/ir.model.access.csv',
+        'reports/sale_order_report_templates.xml',
+        'reports/sale_order_reports.xml',
         'views/assets.xml',
+        'views/canteen_menus.xml',
+        'views/sale_order_views.xml',
         'views/product_template_views.xml',
         'views/canteen_order_portal_templates.xml',
+        'wizards/sale_order_report_canteen_wizard_views.xml',
         'data/ir_cron_data.xml',
     ],
 }
