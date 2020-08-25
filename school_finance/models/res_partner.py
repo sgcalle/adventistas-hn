364f3ed4f30336e07bb57dd043929aa5cf400d92
rainierking <rainierking@gitlab.com>
2020-08-25 23:21:17 +0800

    Return developments for school_finance

--- school_finance/models/res_partner.py
+++ school_finance/models/res_partner.py
@@ -15,6 +15,30 @@ class SchoolFinance(models.Model):
     family_res_finance_ids = fields.One2many("school_finance.financial.res.percent", 'partner_id',
                                              string="Family resposability")
     student_invoice_ids = fields.One2many("account.move", "student_id", string="Student Invoices")
+    invoice_amount_untaxed_signed = fields.Monetary(string="Total Tax Ecluded",
+        compute="_compute_invoice_totals")
+    invoice_amount_tax_signed = fields.Monetary(string="Total Tax",
+        compute="_compute_invoice_totals")
+    invoice_amount_total_signed = fields.Monetary(string="Total Total",
+        compute="_compute_invoice_totals")
+    invoice_amount_residual_signed = fields.Monetary(string="Total Amount Due",
+        compute="_compute_invoice_totals")
+    student_invoice_amount_untaxed_signed = fields.Monetary(string="Student Total Tax Ecluded",
+        compute="_compute_invoice_totals")
+    student_invoice_amount_tax_signed = fields.Monetary(string="Student Total Tax",
+        compute="_compute_invoice_totals")
+    student_invoice_amount_total_signed = fields.Monetary(string="Student Total Total",
+        compute="_compute_invoice_totals")
+    student_invoice_amount_residual_signed = fields.Monetary(string="Student Total Amount Due",
+        compute="_compute_invoice_totals")
+    family_invoice_amount_untaxed_signed = fields.Monetary(string="Family Total Tax Ecluded",
+        compute="_compute_invoice_totals")
+    family_invoice_amount_tax_signed = fields.Monetary(string="Family Total Tax",
+        compute="_compute_invoice_totals")
+    family_invoice_amount_total_signed = fields.Monetary(string="Family Total Total",
+        compute="_compute_invoice_totals")
+    family_invoice_amount_residual_signed = fields.Monetary(string="Family Total Amount Due",
+        compute="_compute_invoice_totals")
 
     def _check_category_sum(self):
         for record in self:
@@ -55,7 +79,24 @@ class SchoolFinance(models.Model):
             if record.is_company:
                 invoices = self.member_ids.invoice_ids + self.member_ids.student_invoice_ids + self.invoice_ids
             record.family_invoice_ids = invoices
-
+    
+    def _compute_invoice_totals(self):
+        for partner in self:
+            invoice_ids = partner.invoice_ids.filtered(lambda i: i.state != "cancel" and i.type in ["out_invoice","out_refund","out_receipt"])
+            student_invoice_ids = partner.student_invoice_ids.filtered(lambda i: i.state != "cancel" and i.type in ["out_invoice","out_refund","out_receipt"])
+            family_invoice_ids = partner.family_invoice_ids.filtered(lambda i: i.state != "cancel" and i.type in ["out_invoice","out_refund","out_receipt"])
+            partner.invoice_amount_untaxed_signed = sum(invoice_ids.mapped("amount_untaxed_signed"))
+            partner.invoice_amount_tax_signed = sum(invoice_ids.mapped("amount_tax_signed"))
+            partner.invoice_amount_total_signed = sum(invoice_ids.mapped("amount_total_signed"))
+            partner.invoice_amount_residual_signed = sum(invoice_ids.mapped("amount_residual_signed"))
+            partner.student_invoice_amount_untaxed_signed = sum(student_invoice_ids.mapped("amount_untaxed_signed"))
+            partner.student_invoice_amount_tax_signed = sum(student_invoice_ids.mapped("amount_tax_signed"))
+            partner.student_invoice_amount_total_signed = sum(student_invoice_ids.mapped("amount_total_signed"))
+            partner.student_invoice_amount_residual_signed = sum(student_invoice_ids.mapped("amount_residual_signed"))
+            partner.family_invoice_amount_untaxed_signed = sum(family_invoice_ids.mapped("amount_untaxed_signed"))
+            partner.family_invoice_amount_tax_signed = sum(family_invoice_ids.mapped("amount_tax_signed"))
+            partner.family_invoice_amount_total_signed = sum(family_invoice_ids.mapped("amount_total_signed"))
+            partner.family_invoice_amount_residual_signed = sum(family_invoice_ids.mapped("amount_residual_signed"))
 
 class FinacialResponsabilityPercent(models.Model):
     _name = "school_finance.financial.res.percent"
