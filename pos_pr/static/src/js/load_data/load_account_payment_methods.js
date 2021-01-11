aa8240e6a8ad25034ccb99b5190bbdd1d1468fdf
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2021-01-11 15:48:51 +0100

    [FIX] Unused old code

--- pos_pr/static/src/js/load_data/load_account_payment_methods.js
+++ pos_pr/static/src/js/load_data/load_account_payment_methods.js
@@ -3,37 +3,7 @@ odoo.define("pos_pr.load_data.account_payment_methodss", function (require) {
     const models = require("point_of_sale.models");
     const PosDB = require("point_of_sale.DB");
 
-    PosDB.include({
-        init: function (options) {
-            this._super(options);
-            this.account_payment_methods = [];
-            this.account_payment_method_by_id = {};
-        },
-
-        add_account_payment_methods: function (accountPaymentMethods) {
-            this.accountPaymentMethods = accountPaymentMethods;
-            let self = this;
-            _.each(accountPaymentMethods, function (accountPaymentMethod) {
-                self.account_payment_method_by_id[accountPaymentMethod.id] = accountPaymentMethod;
-            });
-        },
-
-    });
-
     // Load payment journals
-    models.load_models([
-        {
-            model: "account.payment.method",
-            fields: ["name"],
-            domain: [
-                ["payment_type", "=", "inbound"]
-            ],
-            loaded: function (self, accountPaymentMethods) {
-                self.db.add_account_payment_methods(accountPaymentMethods);
-            }
-        }
-    ]);
-
     models.load_models([
         {
             model: "pos.payment.method",
