70b583060eea7cecd37bf4b1c348b0009f6d83b1
rainierking <rainierking@gitlab.com>
2021-01-26 21:59:57 +0800

    [IMP] hr_payroll_account_extends: Add option in invoice payment scope for Overdue only at the end of the period

--- hr_payroll_account_extends/models/hr_payroll_structure_type.py
+++ hr_payroll_account_extends/models/hr_payroll_structure_type.py
@@ -8,6 +8,7 @@ class HrPayrollStructureType(models.Model):
     invoice_payment_scope = fields.Selection(string="Invoice Payment Scope",
         selection=[
             ("unpaid", "All Unpaid"),
-            ("overdue", "Overdue Only")],
+            ("overdue", "Overdue Only"),
+            ("overdue_end_of_period", "Overdue only at the end of the period")],
         default="unpaid",
         help="Which unpaid invoice amount should be considered for payslip deduction. Leave empty if you don't want to deduct invoices")
